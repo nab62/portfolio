3f7f6799a8a12cbb174fde987704f45f01c9e072
nabina <thapasumitmay@gmail.com>
2024-07-19 16:30:47 +0545

    first commit

---
+++ src/data.js
@@ -0,0 +1,16 @@
+const data = {
+    name: "nabina",
+    githubLink: "https://github.com/nab62" ,
+    email : "thapasumitmay@gmail.com",
+    hobbies : [
+        "Movies",
+        "Travelling",
+        "Football"
+    ],
+    jobTitle : "Software Engineer",
+    degree: "Bachelor's in Computer Engineering",
+    campus: "Pulchowk Campus",
+    location: "Nepal",
+    spokenLanguages: "Nepali,English",
+}
+export default data;
